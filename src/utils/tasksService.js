57f029cdbdd3d36491b88e3e152f22588f20cc3a
ItzaCarrillo <vichytacarrillo@gmail.com>
2020-05-15 01:35:47 -0500

    masterbackup: new folder utils with tasksService file

---
+++ src/utils/tasksService.js
@@ -0,0 +1,45 @@
+const token = process.env.API_TOKEN;
+
+// aquí vamos a extraer la base de la API
+// en algún punto esta constante se va a usar en varios módulos
+// así que podrían moverla a un archivo de constantes.
+const API_BASE_URL='https://cubosv2.4040.wtf/api';
+const headers = {
+  authtoken: token
+};
+
+// Obtener la tarea con un ID específico
+async function getTask(id) {
+  const request = await fetch(`${API_BASE_URL}/tasks/${id}`, {
+    headers,
+    method: 'GET'
+  });
+  const data = await request.json();
+  return data;
+}
+
+// Buscar todas las tareas de los proyectos (simple ejemplo)
+async function searchAllTasks() {
+  const request = await fetch(`${API_BASE_URL}/tasks/search/?`, {
+    headers,
+    method: 'GET'
+  });
+  const data = await request.json();
+  return data;
+}
+
+async function searchAllLeads(){
+    const request = await fetch(`${API_BASE_URL}/leads/search/?`, {
+        headers,
+        method: 'GET'
+      });
+      const data = await request.json();
+      return data;    
+}
+
+// Aquí vamos a exponer sólo los métodos que necesitamos
+module.exports = {
+  getTask,
+  searchAllTasks,
+  searchAllLeads
+}
